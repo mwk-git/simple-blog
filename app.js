b32fa9afbabfa286929b69a4a69f392bd61a7be9
Michał Wieczorek <mwk123456@gmail.com>
2022-03-15 19:50:12 +0100

    Added core project

---
+++ app.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+const express = require("express");
+
+const blogRoutes = require("./routes/blog");
+const db = require("./data/database");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use(blogRoutes);
+
+app.use(function (error, req, res, next) {
+
+  console.log(error);
+  res.status(500).render("500");
+});
+
+db.connectToDatabase().then(function () {
+  app.listen(3000);
+});
---
+++ public/scripts/comments.js
@@ -0,0 +1,80 @@
+const loadCommentsBtnElement = document.getElementById("load-comments-btn");
+const commentsSectionElement = document.getElementById("comments");
+const commentsFormElement = document.querySelector("#comments-form form");
+const commentTitleElement = document.getElementById("title");
+const commentTextElement = document.getElementById("text");
+
+function createCommentsList(comments) {
+  const commentListElement = document.createElement("ol");
+
+  for (const comment of comments) {
+    const commentElement = document.createElement("li");
+    commentElement.innerHTML = `<article class="comment-item">
+    <h2>${comment.title}</h2>
+    <p>${comment.text}</p>
+  </article>
+  `;
+
+    commentListElement.appendChild(commentElement);
+  }
+
+  return commentListElement;
+}
+
+async function fetchCommentsForPost() {
+  const postId = loadCommentsBtnElement.dataset.postid;
+  try {
+    const response = await fetch(`/posts/${postId}/comments`);
+
+    if (!response.ok) {
+      alert("Fetching comments failed!");
+      return;
+    }
+    const responseData = await response.json();
+
+    if (responseData && responseData.length > 0) {
+      const commentListElement = createCommentsList(responseData);
+      commentsSectionElement.innerHTML = "";
+      commentsSectionElement.appendChild(commentListElement);
+    } else {
+      commentsSectionElement.firstElementChild.textContent =
+        "We could not find any comments. Maybe add one?";
+    }
+  } catch (error) {
+    alert("Getting comments failed!");
+  }
+}
+
+async function saveComment(event) {
+  event.preventDefault();
+  const postId = loadCommentsBtnElement.dataset.postid;
+
+  const enteredTitle = commentTitleElement.value;
+  const enteredText = commentTextElement.value;
+
+  console.log(enteredTitle);
+  console.log(enteredText);
+
+  const comment = { title: enteredTitle, text: enteredText };
+
+  console.log(comment);
+
+  try {
+    const response = await fetch(`/posts/${postId}/comments`, {
+      method: "POST",
+      body: JSON.stringify(comment),
+      headers: { "Content-type": "application/json" },
+    });
+
+    if (response.ok) {
+      fetchCommentsForPost();
+    } else {
+      alert("Could not send comment!");
+    }
+  } catch (error) {
+    alert("Could not send request - maybe try again later.");
+  }
+}
+
+loadCommentsBtnElement.addEventListener("click", fetchCommentsForPost);
+commentsFormElement.addEventListener("submit", saveComment);
